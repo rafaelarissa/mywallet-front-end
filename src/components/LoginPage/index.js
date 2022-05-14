3c515aaa02b84bfab85b7b7813ba9593bb78b0ff
Rafaela Larissa <93556654+rafaelarissa@users.noreply.github.com>
2022-05-14 16:31:40 -0300

    Update index.js

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import Button from '../Button';
 import Input from '../Input';
